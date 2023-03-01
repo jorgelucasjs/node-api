948e680a4720e3dc11e92549d5ab828d4263614b
jorgelucasjs <jorge.tomasgt@gmail.com>
2023-03-01 15:42:59 +0100

    firt api

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const corsHandler = require('cors')({ origin: true });
+
+
+app.get("/test", async (req, res) => {
+	corsHandler(req, res, async () => {
+		
+		const body = req.body;
+
+		const data = {
+			hello: 'Hello USER'
+		}
+
+		console.log(data);
+
+		res.json(data);
+
+	});
+});
+
+app.post("/test", async (req, res) => {
+	corsHandler(req, res, async () => {
+		
+		const body = req.body;
+
+		console.log('TESTE DE API, POST');
+
+		res.json(body);
+
+	});
+});
+
+app.listen('5000', () => console.log('Server is running'));
